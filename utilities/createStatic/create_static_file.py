9aa3d2af2b1ccd7d9e108686942d086ea23e359c
richtnik <niklas.richter.1@hu-berlin.de>
2020-12-08 13:52:26 +0100

    adjusted shp and dem path

--- utilities/createStatic/create_static_file.py
+++ utilities/createStatic/create_static_file.py
@@ -14,9 +14,9 @@ tile = True
 aggregate = True
 
 ### input digital elevation model (DEM)
-dem_path_tif = static_folder + 'DEM/n30_e090_3arc_v2.tif'
+dem_path_tif = static_folder + 'DEM/ALOS_N029E102_AVE_DSM.tif'
 ### input shape of glacier or study area, e.g. from the Randolph glacier inventory
-shape_path = static_folder + 'Shapefiles/Zhadang_RGI6.shp'
+shape_path = static_folder + 'Shapefiles/abramov_rgi.shp'
 ### path were the static.nc file is saved
 output_path = static_folder + 'Zhadang_static.nc'
 
