d56a6f4f4f5e3684aa959f076dfd9f66ff377c78
Niklas Richter <niklas.richter.1@hu-berlin.de>
2021-01-13 20:18:27 +0100

    Adjusted files

---
+++ utilities/aws2cosipy/slurm_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#SBATCH --job-name="AWS2CO"
+#SBATCH --qos=short
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --chdir=/data/scratch/richteny/thesis/cosipy/utilities/aws2cosipy/
+#SBATCH --account=morsanat
+#SBATCH --error=slurm_run.err
+##SBATCH --partition=computehm
+#SBATCH --output=slurm_run.out
+#SBATCH --mail-type=NONE
+
+
+# The batch system should use the current directory as working directory.
+
+
+module load anaconda/2019.07 
+
+python aws2cosipy.py -c ../../data/input/Abramov/Abramov_ERA5L_1981_2019.csv -o ../../data/input/Abramov/Abramov_ERA5L_1981_2016.nc -s ../../data/static/Abramov_static.nc 
