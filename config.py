85b057a82cc8c905348ec0657006e240927f14b3
Niklas Richter <niklas.richter.1@hu-berlin.de>
2021-01-21 11:22:05 +0100

    Adjustments to config etc.

--- config.py
+++ config.py
@@ -7,8 +7,8 @@
 # SIMULATION PERIOD 
 #-----------------------------------
 # Abramov
-time_start = '2016-08-29T06:00'
-time_end   = '2016-09-30T00:00'
+time_start = '2009-01-01T00:00'
+time_end   = '2019-12-31T23:00'
 
 # Hintereisferner
 #time_start = '2018-09-17T08:00'
@@ -23,7 +23,9 @@ time_end_str=(time_end[0:10]).replace('-','')
 data_path = './data/'
 
 # Zhadang example
-input_netcdf= 'Abramov/Abramov_n_agg_ERA5L_06_09_2016.nc'
+# n_agg version -> 'Abramov/Abramov_n_agg_ERA5L_06_09_2016.nc'
+# agg version ->  'Abramov/Abramov_ERA5L_1981_2019.nc'
+input_netcdf= 'Abramov/Abramov_ERA5L_1981_2019.nc'
 output_netcdf = 'Abramov_n_agg_ERA5L_'+time_start_str+'-'+time_end_str+'.nc'
 
 # Hintereisferner example
