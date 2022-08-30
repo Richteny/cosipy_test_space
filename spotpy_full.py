591ffb3c6b9dcb8f210595848d34408a934ae6da
Niklas Richter <niklas.richter.1@hu-berlin.de>
2022-08-30 14:12:36 +0200

    Bringing it all up to date

--- spotpy_full.py
+++ spotpy_full.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import spotpy  # Load the SPOT package into your working storage
 from spotpy.parameter import Uniform
 from spotpy import analyser  # Load the Plotting extension
-from spotpy.objectivefunctions import rmse
+from spotpy.objectivefunctions import rmse, mae
 from COSIPY import main
 from constants import *
 from config import *
@@ -27,30 +27,35 @@ obs = obs[(obs['LS_DATE'] > time_start_dt) & (obs['LS_DATE'] <= time_end_dt)]
 obs.set_index('LS_DATE', inplace=True)
 
 ## Load variable dates from manual selection ##
-var_dates_df = pd.read_csv("./data/input/Abramov/snowlines/variable_snowline_modelled_dates.csv")
-var_dates_df['Variable_Dates'] = pd.to_datetime(var_dates_df['Variable_Dates'])
-obs = obs[obs.index.isin(var_dates_df['Variable_Dates'])]
+#var_dates_df = pd.read_csv("./data/input/Abramov/snowlines/variable_snowline_modelled_dates.csv")
+#var_dates_df['Variable_Dates'] = pd.to_datetime(var_dates_df['Variable_Dates'])
+#obs = obs[obs.index.isin(var_dates_df['Variable_Dates'])]
 #count=-1
+
+## Load parameter list ##
+param_list = pd.read_csv('/data/scratch/richteny/thesis/cosipy_test_space/param_files/2D_Wohlfahrt/cosipy_par_smpl.csv')
+print(param_list.head(3))
+fromlist=True
 
 class spot_setup:
     # defining all parameters and the distribution
-    param = lr_T, lr_RRR, lr_RH, RRR_factor, alb_ice, alb_snow, alb_firn,\
-            albedo_aging, albedo_depth = [
-        Uniform(low=-0.007, high=-0.005),  # lr_temp
-        Uniform(low=0, high=0.00017),  # lr_prec
-        Uniform(low=0, high=0.1), #lr RH2 -> in percent so from 0 to 1 % no prior knowledge for this factor
-        Uniform(low=1.25, high=1.7, step=0.01), #1.235, high=1.265
-        Uniform(low=0.18, high=0.4, step=0.01),  # alb ice
-        Uniform(low=0.65, high=0.9, step=0.01),  # alb snow
-        Uniform(low=0.4, high=0.65, step=0.01), #alb_firn
-        Uniform(low=5, high=23, step=1), #effect of age on snow albedo (days)
-        Uniform(low=3, high=8, step=1) #effect of snow depth on albedo (cm) 
-        ]
+    #param = lr_T, lr_RRR, lr_RH, RRR_factor, alb_ice, alb_snow, alb_firn,\
+    #        albedo_aging, albedo_depth = [
+    #    Uniform(low=-0.007, high=-0.005),  # lr_temp
+    #    Uniform(low=0, high=0.00017),  # lr_prec
+    #    Uniform(low=0, high=0.1), #lr RH2 -> in percent so from 0 to 1 % no prior knowledge for this factor
+    #    Uniform(low=1.25, high=1.8, step=0.01), #1.235, high=1.265
+    #    Uniform(low=0.18, high=0.4, step=0.01),  # alb ice
+    #    Uniform(low=0.65, high=0.9, step=0.01),  # alb snow
+    #    Uniform(low=0.4, high=0.65, step=0.01), #alb_firn
+    #    Uniform(low=5, high=23, step=1), #effect of age on snow albedo (days)
+    #    Uniform(low=3, high=8, step=1) #effect of snow depth on albedo (cm) 
+    #    ]
 
     # Number of needed parameter iterations for parametrization and sensitivity analysis
     M = 4  # inference factor (default = 4)
     d = 2  # frequency step (default = 2)
-    k = len(param)  # number of parameters
+    k = 9 #len(param)  # number of parameters
 
     par_iter = (1 + 4 * M ** 2 * (1 + (k - 2) * d)) * k
 
@@ -58,7 +63,34 @@ class spot_setup:
         self.obj_func = obj_func
         self.obs = obs
         self.count = count
+        if fromlist:
+            print("Getting parameters from list.")
+            self.params = [spotpy.parameter.List('lr_T',param_list['parlr_T'].tolist()),
+                           spotpy.parameter.List('lr_RRR',param_list['parlr_RRR'].tolist()),
+                           spotpy.parameter.List('lr_RH',param_list['parlr_RH'].tolist()),
+                           spotpy.parameter.List('RRR_factor',param_list['parRRR_factor'].tolist()),
+                           spotpy.parameter.List('alb_ice',param_list['paralb_ice'].tolist()),
+                           spotpy.parameter.List('alb_snow',param_list['paralb_snow'].tolist()),
+                           spotpy.parameter.List('alb_firn',param_list['paralb_firn'].tolist()),
+                           spotpy.parameter.List('albedo_aging',param_list['paralbedo_aging'].tolist()),
+                           spotpy.parameter.List('albedo_depth',param_list['paralbedo_depth'].tolist())]
+        else:
+            print("Setting parameters.")
+            self.params = [spotpy.parameter.Uniform('lr_T',[-0.007,-0.005]),
+                           spotpy.parameter.Uniform('lr_RRR',[0, 0.00017]),
+                           spotpy.parameter.Uniform('lr_RH',[0, 0.1]),
+                           spotpy.parameter.Uniform('RRR_factor',[1.25,1.8,0.01]), #1.235, high=1.265
+                           spotpy.parameter.Uniform('alb_ice',[0.18,0.4,0.01]),
+                           spotpy.parameter.Uniform('alb_snow',[0.65,0.9,0.01]),
+                           spotpy.parameter.Uniform('alb_firn',[0.4,0.65,0.01]),
+                           spotpy.parameter.Uniform('albedo_aging',[5,23,1]),
+                           spotpy.parameter.Uniform('albedo_depth',[3,8,1])]
+
         print("Initialised.")
+
+    def parameters(self):
+        return spotpy.parameter.generate(self.params)
+
 
     def simulation(self, x):
         if isinstance(self.count,int):
@@ -81,18 +113,20 @@ class spot_setup:
             eval = np.delete(evaluation.SC_median.values, np.argwhere(np.isnan(simulation)))
             sim = simulation[~np.isnan(simulation)]
             like = -rmse(eval, sim) #set minus before rmse if trying to maximize, depends on algorithm
+            like2 = -mae(eval,sim)
             print("RMSE is: ", like)
+            print("MAE is: ", like2)
         else:
             # Way to ensure flexible spot setup class
             like = self.obj_func(evaluation.SC_median.values, simulation.Med_TSL.values)
-        return like
+        return [like,like2]
 
  
 
 def psample(obs, rep=10, count=None, dbname='cosipy_par_smpl', dbformat="csv", interf=4, freqst=2, ngs=2,
-            algorithm='mcmc', savefig=True):
-
-    #set seed to make results reproducable
+            algorithm='mc', savefig=True):
+    #try lhs which allows for multi-objective calibration which mcmc here does not
+    #set seed to make results reproducable, -> for running from list only works with mc
     np.random.seed(42)
     random.seed(42)
 
