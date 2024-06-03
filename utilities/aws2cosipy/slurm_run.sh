425e1ff9c888175f832c7785d5dc671b6f04fd7b
Richteny <jnrichter@gmx.net>
2024-06-03 11:12:57 +0200

    Updating various scripts

--- utilities/aws2cosipy/slurm_run.sh
+++ utilities/aws2cosipy/slurm_run.sh
@@ -15,10 +15,11 @@
 # The batch system should use the current directory as working directory.
 
 
-module load anaconda/2019.07 
-#conda activate cosipy_test
+#module load anaconda/2019.07 
+conda activate horayzon_all
 
 #python aws2cosipy.py -c ../../data/input/Abramov/Abramov_ERA5mod_spinup_Forcing_2009-2020.csv -o ../../data/input/Abramov/Abramov_300m_ERA5mod_spinup_2009-2020.nc -s ../../data/static/Abramov_300m_static_test.nc
 
 #python aws2cosipy_saveonlyG.py -c ../../data/input/Abramov/Abramov_ERA5_1999_2021.csv -o ../../data/input/Abramov/Abramov_30m_SW_Moelg2009_2009_2010.nc -s ../../data/static/Abramov_30m_static_25kmhrzd.nc -b 20100101 -e 20101231
-python createLUT_crop.py -c ../../data/input/Abramov/Abramov_ERA5mod_spinup_Forcing_2009-2020.csv -o ../../data/input/Abramov/Abramov_30m_SW_Moelg_2014-2015.nc -s ../../data/static/Abramov_30m_static_25kmhrzd.nc -b 20140101 -e 20150101 
+#python createLUT_crop.py -c ../../data/input/Abramov/Abramov_ERA5mod_spinup_Forcing_2009-2020.csv -o ../../data/input/Abramov/Abramov_30m_SW_Moelg_2014-2015.nc -s ../../data/static/Abramov_30m_static_25kmhrzd.nc -b 20140101 -e 20150101 
+python cosmo2cosipy.py -c ../../data/input/HEF/COSMO_forcing_1999-2010.csv -o ../../data/input/HEF/HEF_COSMO_30m_Moelg2009_1999_2010.nc -s ../../data/static/HEF/HEF_static_raw_crop.nc -b 19990101 -e 20100101 -sx ../../data/static/HEF/HEF_static_raw.nc 
