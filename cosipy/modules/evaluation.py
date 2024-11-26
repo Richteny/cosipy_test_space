b49153b3db607d9c93e5b8f234145f569e659f13
Niklas Richter <niklas.richter@uibk.ac.at>
2024-11-26 19:19:21 +0100

    (feat): updated modules with param. calibration option by including tuple

--- cosipy/modules/evaluation.py
+++ cosipy/modules/evaluation.py
@@ -11,42 +11,56 @@ from scipy import stats
 from datetime import datetime
 from numba import njit
 
-def evaluate(stake_names, stake_data, df_):
+def evaluate(stake_names, stake_data, df):
     """Evaluate the simulation using stake measurements.
+
+    Implemented stake evaluation methods:
+
+        - **rmse**: RMSE of simulated mass balance.
 
     Args:
         stake_names (list): Stake IDs.
         stake_data (pd.Dataframe): Stake measurements.
         df (pd.Dataframe): Simulated mass balance and snow height.
-    
+
     Returns:
-        Statistical evaluation.
+        float or None: Statistical evaluation.
     """
 
-    if Config.eval_method == 'rmse':
-        stat = rmse(stake_names, stake_data, df_)
+    if Config.eval_method == "rmse":
+        stat = rmse(stake_names, stake_data, df)
     else:
         stat = None
-       
+
     return stat
 
 
-def rmse(stake_names, stake_data, df_):
-    if (Config.obs_type=='mb'):
-        rmse = ((stake_data[stake_names].subtract(df_['mb'],axis=0))**2).mean()**.5
-    elif (Config.obs_type=='snowheight'):
-        rmse = ((stake_data[stake_names].subtract(df_['snowheight'],axis=0))**2).mean()**.5
-    else:
-        msg = f'RMSE not implemented for obs_type="{Config.obs_type}" in config.py.'
-        raise NotImplementedError(msg)
-    return rmse
+def rmse(stake_names: list, stake_data, df) -> float:
+    """Get RMSE of simulated stake measurements.
 
+    Args:
+        stake_names: Stake IDs.
+        stake_data (pd.Dataframe): Stake measurements.
+        df (pd.Dataframe): Simulated mass balance and snow height.
+
+    Returns:
+        RMSE of simulated measurements.
+    """
+    if Config.obs_type not in ["mb", "snowheight"]:
+        msg = f'RMSE not implemented for obs_type="{Config.obs_type}" in config.toml.'
+        raise NotImplementedError(msg)
+    else:
+        rmse = (
+            (stake_data[stake_names].subtract(df[Config.obs_type], axis=0))
+            ** 2
+        ).mean() ** 0.5
+
+    return rmse
 
 
 def resample_output(cos_output):
     times = datetime.now()
-    ds = cos_output
-    ds = ds[['SNOWHEIGHT','HGT','MASK']]
+    ds = cos_output[['SNOWHEIGHT','HGT','MASK']]
     #ds_daily = ds.resample(time='1d', keep_attrs=True).mean(dim='time')
     df_daily = ds.to_dataframe().groupby([pd.Grouper(level='time',freq='1d'),
                                           pd.Grouper(level='lat'),
